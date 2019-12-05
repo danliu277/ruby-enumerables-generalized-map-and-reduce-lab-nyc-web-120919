12d838177284b9169eb5ba3742ff19abff6c1e2e
Daniel Liu <danliu277@gmail.com>
2019-12-05 00:01:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map(array)
   index = 0
   result = []
   while index < array.length do
-    result.push(yield(aray[index]))
+    result.push(yield(array[index]))
     index += 1
   end
   return result
